13908cd06320e3c13709b0e5238cc103fcf29d73
Nikita Detkov <ndsgfk@gmail.com>
2025-05-18 16:35:47 +0000

    Minor bug fix

---
+++ initialize_project.sh
@@ -0,0 +1,6 @@
+mkdir ~/.kaggle
+mv test_file.json ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
+mkdir input
+kaggle competitions download -p input -c birdclef-2025
+unzip input/birdclef-2025.zip -d input/birdclef-2025/
